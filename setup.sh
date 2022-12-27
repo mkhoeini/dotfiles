a0be535b533ee72321a04b32f6a01f2030ac9a4b
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2022-12-27 21:49:45 +0100

    Change the deps install to use shell script tangle strategy

--- setup.sh
+++ setup.sh
@@ -12,6 +12,75 @@ fi
 
 home_links/.local/bin/run_org README.org tldr
 
+formula_list='antigen
+asdf
+babashka
+bat
+bottom
+clojure
+clojurescript
+coreutils
+cowsay
+curlie
+direnv
+exa
+fd
+fortune-mod
+fzf
+git
+git-gui
+gnu-sed
+hub
+hyperfine
+ijq
+jq
+lolcat
+mob
+neovide
+neovim
+ponysay
+procs
+ripgrep
+rlwrap
+starship
+stow
+watchexec
+zellij
+zoxide
+zsh'
+cask_list='alacritty
+browserosaurus
+coconutbattery
+firefox
+font-droidsansmono-nerd-font
+font-iosevka-nerd-font
+font-jetbrains-mono-nerd-font
+font-juliamono
+google-cloud-sdk
+hammerspoon
+iina
+intellij-idea-ce
+itsycal
+telegram
+tomatobar
+vimr
+visual-studio-code'
+tap_list='borkdude/brew
+homebrew/cask-drivers
+homebrew/cask-fonts
+homebrew/cask-versions
+mkhoeini/tap
+remotemobprogramming/brew'
+while read -r tap || [[ -n "$tap" ]]; do
+  brew tap "$tap";
+done <<< $tap_list
+while read -r formula || [[ -n "$formula" ]]; do
+  brew install "$formula";
+done <<< $formula_list
+while read -r cask || [[ -n "$cask" ]]; do
+  brew install --cask "$cask";
+done <<< $cask_list
+
 if [[ ! -e ~/.emacs.d ]]; then
     echo "Installing Spacemacs";
     git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d;
