c1208b053dca93478762d6315670fc2d1752c08d
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2023-03-28 15:29:27 +0200

    Add new hammerspoon shortcuts for window management

--- home_links/.hammerspoon/init.lua
+++ home_links/.hammerspoon/init.lua
@@ -8,6 +8,9 @@ spoon.SpoonInstall:andUse("CircleClock", {})
 spoon.SpoonInstall:andUse("ClipboardTool", { start = true })
 spoon.SpoonInstall:andUse("Emojis", {})
 
+-- Make setFrame behave more correctly. e.g. terminal windows
+hs.window.setFrameCorrectness = true
+
 function toggleEmojis()
   if spoon.Emojis.chooser:isVisible() then
     spoon.Emojis.chooser:hide()
@@ -30,14 +33,16 @@ end
 
 spoon.SpoonInstall:andUse("KSheet", {})
 
-function resizeWindowToA4()
-  local phi = 4/3 -- (1 + math.sqrt(2)) / 2
+local phi = 4/3 -- (1 + math.sqrt(2)) / 2
+local menubarHeight = 25
+local windowGap = menubarHeight * 1.44
 
+function resizeWindowToA4()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen():frame()
 
-  local max_h = screen.h * 0.85
+  local max_h = screen.h * 0.95
   local h = math.min(f.w * phi, max_h)
   local w = h / phi
 
@@ -49,6 +54,70 @@ function resizeWindowToA4()
   end
 
   win:setFrame(f)
+end
+
+function moveAndResizeWindowLeft()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+
+  local f = screen:fromUnitRect("0.1 0 0.55 1")
+
+  f.x = f.x + windowGap
+  f.y = f.y + windowGap + menubarHeight
+  f.w = f.w - 2 * windowGap
+  f.h = f.h - 2 * windowGap - menubarHeight
+
+  win:setFrame(f)
+end
+
+function moveAndResizeWindowRight()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+
+  local f = screen:fromUnitRect("0.55 0 1 1")
+
+  f.x = f.x + windowGap
+  f.y = f.y + windowGap + menubarHeight
+  f.w = f.w - 2 * windowGap
+  f.h = f.h - 2 * windowGap - menubarHeight
+
+  win:setFrame(f)
+end
+
+function moveToCenter()
+  hs.window.focusedWindow():centerOnScreen()
+end
+
+function moveToCenterAndResize()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+
+  local win_f = win:frame()
+  local f = screen:frame()
+
+  f.x = win_f.x
+  f.y = win_f.y + menubarHeight
+  f.h = f.h * 0.95
+  f.w = f.h / phi
+
+  win:setFrame(f)
+  win:centerOnScreen()
+end
+
+function doubleSize()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+
+  local win_f = win:frame()
+  local f = screen:fromUnitRect("0 0 1 1")
+
+  f.x = win_f.x
+  f.y = win_f.y
+  f.h = f.h * 0.95
+  f.w = f.h / phi * 2 + windowGap
+
+  win:setFrame(f)
+  win:centerOnScreen()
 end
 
 spoon.SpoonInstall:andUse("RecursiveBinder", {})
@@ -63,6 +132,11 @@ local keyMap = spoon.RecursiveBinder.recursiveBind{
     [k('a', 'application')] = function() spoon.KSheet:toggle() end,
   },
   [k('w', 'window')] = {
+    [k('c', 'move to center')] = moveToCenter,
+    [k('C', 'move and resize to center')] = moveToCenterAndResize,
+    [k('d', 'double size')] = doubleSize,
+    [k('h', 'resize to left')] = moveAndResizeWindowLeft,
+    [k('l', 'resize to right')] = moveAndResizeWindowRight,
     [k('p', 'proportionate')] = resizeWindowToA4,
   },
 }
