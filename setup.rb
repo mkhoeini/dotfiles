0cf49102ec97152093af7158cb8d60276c9e3518
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2024-06-08 00:16:00 +0200

    feat: Rewrote everything to be based on ruby

--- setup.rb
+++ setup.rb
@@ -1,4 +1,98 @@
 #!/usr/bin/env ruby
+def exec?(cmd)
+  system("command -v #{cmd} >/dev/null")
+end
+
+if not exec?("brew")
+  puts "Installing HomeBrew"
+  system %q{bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"}
+end
+
+if not exec?('emacs')
+    puts "Installing Emacs"
+    system "brew tap d12frosted/emacs-plus"
+    system "brew install emacs-plus@30 --with-native-comp --with-modern-vscode-icon --with-xwidgets --with-imagemagick --with-poll --with-no-frame-refocus"
+end
+
+if not exec?('stow')
+    puts "Installing GNU Stow"
+    system "brew install stow"
+end
+
+system "stow -t $HOME home_links"
+
+if not File.exist?(File.expand_path "~/dotemacs/doom")
+    puts "Installing Doom Emacs"
+    system "git clone --depth 1 https://github.com/doomemacs/doomemacs ~/dotemacs/doom"
+end
+
+if not File.exist?(File.expand_path "~/.config/nvim")
+    puts "Installing LazyVim"
+    system "git clone https://github.com/LazyVim/starter ~/.config/nvim"
+end
+
+if not File.exist?(File.expand_path "~/.intellimacs")
+    puts "Installing Intellimacs"
+    system "git clone https://github.com/MarcoIeni/intellimacs ~/.intellimacs"
+end
+
+MACOS_DEFAULTS = {
+  # Enable repeat keys
+  ApplePressAndHoldEnabled: "-bool false",
+
+  # opening and closing windows and popovers
+  NSAutomaticWindowAnimationsEnabled: "-bool false",
+
+  # smooth scrolling
+  NSScrollAnimationEnabled: "-bool false",
+
+  # showing and hiding sheets, resizing preference windows, zooming windows
+  # float 0 doesn't work
+  NSWindowResizeTime: "-float 0.001",
+
+  # opening and closing Quick Look windows
+  QLPanelAnimationDuration: "-float 0",
+
+  # rubberband scrolling (doesn't affect web views)
+  NSScrollViewRubberbanding: "-bool false",
+
+  # resizing windows before and after showing the version browser
+  # also disabled by NSWindowResizeTime -float 0.001
+  NSDocumentRevisionsWindowTransformAnimation: "-bool false",
+
+  # showing a toolbar or menu bar in full screen
+  NSToolbarFullScreenAnimationDuration: "-float 0",
+
+  # scrolling column views
+  NSBrowserColumnAnimationSpeedMultiplier: "-float 0",
+}
+
+# TODO app defaults
+# showing the Dock
+# defaults write com.apple.dock autohide-time-modifier -float 0
+# defaults write com.apple.dock autohide-delay -float 0
+
+# showing and hiding Mission Control, command+numbers
+# defaults write com.apple.dock expose-animation-duration -float 0
+
+# showing and hiding Launchpad
+# defaults write com.apple.dock springboard-show-duration -float 0
+# defaults write com.apple.dock springboard-hide-duration -float 0
+
+# changing pages in Launchpad
+# defaults write com.apple.dock springboard-page-duration -float 0
+
+# at least AnimateInfoPanes
+# defaults write com.apple.finder DisableAllAnimations -bool true
+
+# sending messages and opening windows for replies
+# defaults write com.apple.Mail DisableSendAnimations -bool true
+# defaults write com.apple.Mail DisableReplyAnimations -bool true
+
+MACOS_DEFAULTS.each_pair do |opt, val|
+  system "defaults write -g #{opt} #{val}"
+end
+
 tap_list = <<HEREDOC.gsub(/;.*$/, '').split("\n").map(&:strip).reject(&:empty?)
   borkdude/brew ; babashka
   ;; bufbuild/buf
--- setup.sh
+++ setup.sh
@@ -1,42 +1,8 @@
 #!/usr/bin/env zsh
 set -euxo pipefail
 
-# I forgot the password! ':)
-# eval "$(gpg --decrypt secrets.sh.gpg)"
+# get the secrets
+eval "$(gpg --decrypt secrets.sh.gpg)"
 
-if ! command -v brew >/dev/null; then
-    echo "Installing HomeBrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-if ! command -v emacs >/dev/null; then
-    echo "Installing Emacs"
-    brew tap d12frosted/emacs-plus
-    brew install emacs-plus@30 --with-native-comp --with-modern-vscode-icon --with-xwidgets --with-imagemagick --with-poll --with-no-frame-refocus
-fi
-
-if ! command -v stow >/dev/null; then
-    echo "Installing GNU Stow"
-    brew install stow
-fi
-
-stow -t $HOME home_links
-
-if [[ ! -e ~/dotemacs/doom ]]; then
-    echo "Installing Doom Emacs";
-    git clone --depth 1 https://github.com/doomemacs/doomemacs ~/dotemacs/doom;
-fi
-
-if [[ ! -e ~/.config/nvim ]]; then
-    echo "Installing LazyVim";
-    git clone https://github.com/LazyVim/starter ~/.config/nvim;
-fi
-
-if [[ ! -e ~/.intellimacs ]]; then
-    echo "Installing Intellimacs";
-    git clone https://github.com/MarcoIeni/intellimacs ~/.intellimacs;
-fi
-
-defaults write -g ApplePressAndHoldEnabled -bool false
-
+# run the ruby setup
 ./setup.rb
