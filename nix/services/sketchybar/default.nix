2c709b14ca7e6d4a79cad60bb7591cd5ed833675
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2024-08-25 21:30:39 +0200

    feat: update sketchybarrc

--- nix/services/sketchybar/default.nix
+++ nix/services/sketchybar/default.nix
@@ -7,17 +7,15 @@ let
     rev = "3946005f6b1f67357ed83ffc6e53eb9c6e867228";
     ref = "refs/tags/v2.21.0";
   };
+  FelixKratzDotfiles = fetchGit {
+    url = "https://github.com/FelixKratz/dotfiles.git";
+    rev = "1589c769e28f110b1177f6a83fa145235c8f7bd6";
+  };
   plugins = sketchyBarGit + /plugins;
   sketchybarrc = readFile ./sketchybarrc;
 in
 {
   enable = true;
   extraPackages = [ pkgs.babashka ];
-  config = ''
-    #!/usr/bin/env bb
-
-    (def PLUGIN_DIR "${plugins}")
-
-    ${sketchybarrc}
-  '';
+  config = replaceStrings [ "$PLUGINS" ] [ plugins ] sketchybarrc;
 }
